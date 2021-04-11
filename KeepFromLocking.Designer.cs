3eb22a1c4151639f0234f7f25ca559963e023444
bhill10gh <barry.hill@velir.com>
2021-04-10 21:05:03 -0400

    Initial Commit

---
+++ KeepFromLocking.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace KeepFromTimingOut
+{
+    partial class KeepFromLocking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KeepFromLocking));
+            this.lblRunStatus = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.Every4Minutes = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblRunStatus
+            // 
+            this.lblRunStatus.AutoSize = true;
+            this.lblRunStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRunStatus.Location = new System.Drawing.Point(86, 79);
+            this.lblRunStatus.Name = "lblRunStatus";
+            this.lblRunStatus.Size = new System.Drawing.Size(101, 24);
+            this.lblRunStatus.TabIndex = 5;
+            this.lblRunStatus.Text = "No Runing";
+            this.lblRunStatus.TextChanged += new System.EventHandler(this.lblRunStatus_TextChanged);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(34, 23);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(213, 34);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Visible = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(34, 23);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(213, 34);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Every4Minutes
+            // 
+            this.Every4Minutes.Tick += new System.EventHandler(this.Every4Minutes_Tick);
+            // 
+            // KeepFromLocking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(284, 133);
+            this.Controls.Add(this.lblRunStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(300, 172);
+            this.MinimumSize = new System.Drawing.Size(300, 172);
+            this.Name = "KeepFromLocking";
+            this.Text = "KeepFromLocking";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRunStatus;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer Every4Minutes;
+    }
+}
---
+++ KeepFromLocking.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows;
+using System.Runtime.InteropServices;
+using KeepFromTimingOut;
+
+namespace KeepFromTimingOut
+{
+    public partial class KeepFromLocking : Form
+    {
+        bool KeepGoing = false;
+        int pointPos = -1;
+        private const uint MOUSEEVENTF_MOVE = 1;
+
+        public KeepFromLocking()
+        {
+            InitializeComponent();
+
+
+            GlobalHotKey.RegisterHotKey("Alt + Shift + Q", () => Stop_KeyPressed());
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto, CallingConvention = System.Runtime.InteropServices.CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, uint dwExtraInfo);
+
+        public void StartTimer()
+        {
+            Every4Minutes.Interval = 1000;
+            Every4Minutes.Tick += new EventHandler(MoveNow);
+
+            pointPos = pointPos * -1;
+
+            Cursor.Position = new System.Drawing.Point(Cursor.Position.X + pointPos, Cursor.Position.Y + pointPos);
+            lblRunStatus.Text = "Running";
+            Every4Minutes.Start();
+        }
+
+        void MoveNow(object sender, EventArgs e)
+        {
+            POINT point = KeepFromLocking.GetCursorPosition();
+            uint x = (uint)point.X, y = (uint)point.Y;
+            if(x > 1500 && y > 800)
+            {
+                this.Cursor = new Cursor(Cursor.Current.Handle);
+                Cursor.Position = new System.Drawing.Point(100, 100);
+            }
+            else
+            {
+                mouse_event(MOUSEEVENTF_MOVE, 1, 1, 0, 0);
+            }
+            //Cursor.Position = new Point(Cursor.Position.X + pointPos, Cursor.Position.Y + pointPos);
+            
+            //System.Threading.Thread.Sleep(60000);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            KeepGoing = true;
+            StartTimer();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            KeepGoing = false;
+        }
+
+        private void Every4Minutes_Tick(object sender, EventArgs e)
+        {
+            if (!KeepGoing)
+            {
+                Every4Minutes.Stop();
+                lblRunStatus.Text = "Not Running";
+                lblRunStatus.Visible = true;
+                return;
+            }
+
+            lblRunStatus.Visible = !lblRunStatus.Visible;
+        }
+
+
+        /// <summary>
+        /// Retrieves the cursor's position, in screen coordinates.
+        /// </summary>
+        /// <see>See MSDN documentation for further information.</see>
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        public static POINT GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+            // NOTE: If you need error handling
+            // bool success = GetCursorPos(out lpPoint);
+            // if (!success)
+
+            return lpPoint;
+        }
+
+        private void lblRunStatus_TextChanged(object sender, EventArgs e)
+        {
+            if(lblRunStatus.Text == "Not Running")
+            {
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+                btnStart.Visible = true;
+                btnStop.Visible = false;
+                lblRunStatus.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                btnStart.Visible = false;
+                btnStop.Visible = true;
+                lblRunStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void Stop_KeyPressed()
+        {
+            KeepGoing = false;
+        }
+    }
+
+    /// <summary>
+    /// Struct representing a point.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public static implicit operator System.Windows.Point(POINT point)
+        {
+            return new System.Windows.Point(point.X, point.Y);
+        }
+    }
+
+}
+
